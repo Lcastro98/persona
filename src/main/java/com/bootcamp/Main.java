b616dece3871f69ef2fe40fb1dff3242f4b90556
Andrea Castro Manzano <andrea.castro@sofka.local>
2022-10-10 16:30:00 -0500

    Se instancia un objeto persona, se da valor a sus atributos y se muestran en consola

--- src/main/java/com/bootcamp/Main.java
+++ src/main/java/com/bootcamp/Main.java
@@ -3,5 +3,14 @@ package com.bootcamp;
 public class Main {
     public static void main(String[] args) {
 
+        Persona persona = new Persona();
+
+        persona.setEdad(18);
+        persona.setNombre("Juana Martinez");
+        persona.setTelefono("3153304897");
+
+        System.out.println(persona.getEdad());
+        System.out.println(persona.getNombre());
+        System.out.println(persona.getTelefono());
     }
 }
