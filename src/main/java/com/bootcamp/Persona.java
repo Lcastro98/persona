b73b2037cfd7ef16751922bfb48697579a52ec57
Andrea Castro Manzano <andrea.castro@sofka.local>
2022-10-10 16:28:52 -0500

    Se crea la clase persona con sus atributos

--- src/main/java/com/bootcamp/Persona.java
+++ src/main/java/com/bootcamp/Persona.java
@@ -1,4 +1,31 @@
 package com.bootcamp;
 
 public class Persona {
+    private Integer edad;
+    private String nombre;
+    private String telefono;
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
 }
